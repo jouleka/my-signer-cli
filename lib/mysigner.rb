feacb555b5eb76709f53ddc4a76130ec6af858b2
Jurgen Leka <lekacoding@gmail.com>
2025-10-01 16:02:56 +0200

    project init

---
+++ lib/mysigner.rb
@@ -0,0 +1,6 @@
+require "mysigner/version"
+
+module Mysigner
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mysigner_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Mysigner do
+  it "has a version number" do
+    expect(Mysigner::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
